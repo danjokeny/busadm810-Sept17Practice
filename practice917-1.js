c921339ebbaaef87140a2b07e2ddca98f025316e
danjokeny <36682157+danjokeny@users.noreply.github.com>
2018-09-18 10:12:37 -0500

    Add files via upload

--- practice917-1.js
+++ practice917-1.js
@@ -2,45 +2,55 @@
 //danny forero
 // sept 17 practice assignment question #1
 /*
-You can write code in Node.js that allows the user to enter a value but it's a bit complex at this stage.  
-So for these exercises, instead of prompting the user for input, you can just declare variables at the top of the file.  
-For example, number 1 compares two numbers so you can declare them like this:
-let temp = 35;
-let  scale = 'C';
-1. Write a function that takes two arguments.  The first is a temperature and the second is the toScale. 
-If the toScale argument is C, convert the temperature from Fahrenheit to Celsius.  
-If the toScale is F, convert the temperature from Celsius to Fahrenheit.  
+2. Write a function that has two arguments and returns the higher of the two.  
+The function should return 'Out of range' if the values are not in the range 40 - 60.  
+Your code should log the result to the console.
 */
 
-//Function to convert from C->F or assume conversion from F->C
-//there is a flaw in this logic in that anything other than C, assumes a conversion from F -> C
-//third example in the test results shows that 10H is assumed as 10C and converts to F
+//Function to convert from C->F or F->C
+//anything in scale other than C or F is returned as an error message
 function ConvertTemp(inTemp, inScale) {
-    //    console.log(inTemp);
-    //    console.log(inScale);
-        if (inScale = "C" ) {
-            newTemp = inTemp * 1.8 + 32
-            newScale = "F"
-        } else {newTemp = (inTemp - 32) * 0.55555556
-                newScale = "C"
-                };
+    if (inScale == 'C' ) {
+        newTemp = inTemp * 1.8 + 32
+        newScale = "F"
+    } else if (inScale == 'F' ) {
+            newTemp = (inTemp - 32) * 0.55555556
+            newScale = "C"
+    }   else { return( inScale + " is an invalid scale, please use C or F")
+
+    };
+
+    return(inTemp + inScale + " = " + newTemp + newScale);
+} ;
     
-        return(inTemp + inScale + " = " + newTemp + newScale);
-    } ;
-    
-    //Call Conversion function three times for three different scenarios
-    
-    //Convert 35C to F
-    let temp = 35;
-    let  scale = 'C';
-    console.log(ConvertTemp(temp,scale));
-    
-    //Convert 0C to F
-    let temp2 = 0;
-    let  scale2 = 'H';
-    console.log(ConvertTemp(temp2,scale2));
-    
-    //Convert 10H (code assumes 10C) to F
-    let temp3 = 10;
-    let  scale3 = 'F';
-    console.log(ConvertTemp(temp3,scale3));
+//Call Conversion function three times for three different scenarios
+
+//Convert 35C to F
+let temp = 35;
+let  scale = 'C';
+console.log(ConvertTemp(temp,scale));
+
+//Convert 0F to C
+temp = 0;
+scale = 'F';
+console.log(ConvertTemp(temp,scale));
+
+//Convert 0C to F
+temp = 0;
+scale = 'C';
+console.log(ConvertTemp(temp,scale));
+
+//Convert 10H (should be an error) to C
+temp = 10;
+scale = 'H';
+console.log(ConvertTemp(temp,scale));
+
+//Convert 100C to F
+temp = 100;
+scale = 'C';
+console.log(ConvertTemp(temp,scale));
+
+//Convert 0C to F
+temp = 0;
+scale = 'C';
+console.log(ConvertTemp(temp,scale));
